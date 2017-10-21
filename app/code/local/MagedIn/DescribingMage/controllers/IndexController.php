50c15756d1c35fad88aaeb50aed30bf76f0e9452
Tiago Sampaio <tiago@tiagosampaio.com>
2017-10-21 19:40:20 -0200

    Source code of module 10, class 05.

--- app/code/local/MagedIn/DescribingMage/controllers/IndexController.php
+++ app/code/local/MagedIn/DescribingMage/controllers/IndexController.php
@@ -48,4 +48,40 @@ class MagedIn_DescribingMage_IndexController extends Mage_Core_Controller_Front_
         Mage::register('magedin_describingmage_rand_value', $rand, true);
     }
 
+
+    public function directoryAction()
+    {
+        /**
+         * Gathering Magento base directories.
+         */
+        $base    = (string) Mage::getBaseDir('base');
+        $app     = (string) Mage::getBaseDir('app');
+        $code    = (string) Mage::getBaseDir('code');
+        $design  = (string) Mage::getBaseDir('design');
+        $etc     = (string) Mage::getBaseDir('etc');
+        $lib     = (string) Mage::getBaseDir('lib');
+        $locale  = (string) Mage::getBaseDir('locale');
+        $media   = (string) Mage::getBaseDir('media');
+        $skin    = (string) Mage::getBaseDir('skin');
+        $var     = (string) Mage::getBaseDir('var');
+        $tmp     = (string) Mage::getBaseDir('tmp');
+        $cache   = (string) Mage::getBaseDir('cache');
+        $log     = (string) Mage::getBaseDir('log');
+        $session = (string) Mage::getBaseDir('session');
+        $upload  = (string) Mage::getBaseDir('upload');
+        $export  = (string) Mage::getBaseDir('export');
+
+        /**
+         * Gathering modules directories.
+         */
+        $moduleEtc         = Mage::getModuleDir('etc', 'Mage_Core');
+        $moduleControllers = Mage::getModuleDir('controllers', 'Mage_Core');
+        $moduleSql         = Mage::getModuleDir('sql', 'Mage_Core');
+        $moduleData        = Mage::getModuleDir('data', 'Mage_Core');
+        $moduleLocaled     = Mage::getModuleDir('locale', 'Mage_Core');
+
+        /** It does not work because it's not listed in Mage_Core_Model_Config::getModuleDir() method. */
+        $moduleModel       = Mage::getModuleDir('model', 'Mage_Core');
+    }
+
 }
