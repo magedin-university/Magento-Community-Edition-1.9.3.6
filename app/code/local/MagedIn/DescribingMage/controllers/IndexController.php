07fea7b0e877a598b2d070ba4d2e26ede7f0a32a
Tiago Sampaio <tiago@tiagosampaio.com>
2017-10-21 19:02:20 -0200

    Source code of module 10, class 03.

--- app/code/local/MagedIn/DescribingMage/controllers/IndexController.php
+++ app/code/local/MagedIn/DescribingMage/controllers/IndexController.php
@@ -3,10 +3,34 @@
 class MagedIn_DescribingMage_IndexController extends Mage_Core_Controller_Front_Action
 {
 
+    /**
+     * Checkout how autoload works in Magento.
+     *
+     * @see Varien_Autoload::autoload($class);
+     */
     public function indexAction()
     {
         /** @var Mage_Catalog_Model_Product $product */
         $product = Mage::getModel('catalog/product');
+    }
+
+
+    /**
+     * Retrieving the version information.
+     */
+    public function versionAction()
+    {
+        $version     = (string) Mage::getVersion();
+        $vertionInfo = (array)  Mage::getVersionInfo();
+    }
+
+
+    /**
+     * Retrieving the edition information (Enterprise Edition or Community Edition).
+     */
+    public function editionAction()
+    {
+        $edition = (string) Mage::getEdition();
     }
 
 }
