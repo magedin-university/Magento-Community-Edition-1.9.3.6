b7315f350ba38a6b54b9092a87429b8c3cbefee5
Tiago Sampaio <tiago@tiagosampaio.com>
2017-10-22 23:09:00 -0200

    Source code of module 12, class 05.

---
+++ app/code/local/MagedIn/Rewrites/Block/Page/Html/Header.php
@@ -0,0 +1,8 @@
+<?php
+
+class MagedIn_Rewrites_Block_Page_Html_Header extends Mage_Page_Block_Html_Header
+{
+
+
+
+}
--- app/code/local/MagedIn/Rewrites/controllers/IndexController.php
+++ app/code/local/MagedIn/Rewrites/controllers/IndexController.php
@@ -3,6 +3,9 @@
 class MagedIn_Rewrites_IndexController extends Mage_Core_Controller_Front_Action
 {
 
+    /**
+     * Overriding a model class.
+     */
     public function modelRewriteAction()
     {
         /** @var Mage_Customer_Model_Customer $customer */
@@ -10,10 +13,25 @@ class MagedIn_Rewrites_IndexController extends Mage_Core_Controller_Front_Action
     }
 
 
+    /**
+     * Overriding a helper class.
+     */
     public function helperRewriteAction()
     {
         /** @var Mage_Customer_Helper_Data $helper */
         $helper = Mage::helper('customer');
+    }
+
+
+    /**
+     * Overriding a block class.
+     */
+    public function blockRewriteAction()
+    {
+        $this->loadLayout();
+
+        /** @var Mage_Page_Block_Html_Header $block */
+        $block = $this->getLayout()->getBlock('header');
     }
 
 }
